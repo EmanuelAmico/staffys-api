7b9a4d8f62493f5a569024c57ff7bab53216562b
elbolaestevez <elbolaestevez@gmail.com>
2023-05-14 17:03:34 -0300

    jsonwebtoken done

---
+++ config/jwt/tokens.ts
@@ -0,0 +1,24 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+interface Payload {
+  name: string;
+}
+const generateToken = (payload: Payload): string => {
+  const secret = process.env.JWT_SECRET as string;
+  const token = jwt.sign({ user: payload.name }, secret, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+const validateToken = (token: string) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET as string);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { generateToken, validateToken };
