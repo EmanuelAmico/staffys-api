f5bd416778cf059d1ae80c56b70247f5f0c4f771
elbolaestevez <elbolaestevez@gmail.com>
2023-06-18 11:56:34 -0300

    Move middleware done

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { AuthService } from "../services/auth.service";
-import { JsonWebTokenError, JwtPayload } from "jsonwebtoken";
-
+// import { JsonWebTokenError, JwtPayload } from "jsonwebtoken";
+// import { CustomRequest } from "../middlewares/auth.middleware";
 class AuthController {
   static async register(
     req: Request<void, string, { name: string }, void>,
@@ -17,36 +17,6 @@ class AuthController {
 
       res.status(200).send(token);
     } catch (error) {
-      next(error);
-    }
-  }
-
-  static login(
-    req: Request,
-    res: Response<string | JwtPayload>,
-    next: NextFunction
-  ) {
-    try {
-      const authorizationHeader = req.headers.authorization;
-
-      if (!authorizationHeader)
-        return res.status(401).send("Authorization token not found");
-
-      const [bearer, token] = authorizationHeader.split(" ");
-
-      if (bearer !== "Bearer" || !token)
-        return res.status(401).send("Invalid authorization header");
-
-      const payload = AuthService.login(token);
-
-      if (!payload) return res.status(401).send("Invalid authorization token");
-
-      res.status(200).send(payload);
-    } catch (error) {
-      if (error instanceof JsonWebTokenError) {
-        return res.status(401).send("Invalid authorization token");
-      }
-
       next(error);
     }
   }
