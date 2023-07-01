3acb6dc0c6b731d231ebac93d0c4b3b80e16f70d
Alex Emanuel Amico <emanuelamicoo@gmail.com>
2023-07-01 16:25:46 -0300

    Add better error handling to API, create APIError class

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ import cors from "cors";
 import morgan from "morgan";
 import { envs } from "./config/env/env.config";
 import connectToDB from "./config/db";
-import History from "./models/History";
 import { allRoutes } from "./routes";
+import { APIError } from "./utils/error.utils";
 
 const { PORT, BACKOFFICE_CLIENT_HOST, DELIVERY_CLIENT_HOST } = envs;
 const app = express();
@@ -18,28 +18,22 @@ const app = express();
 const options: cors.CorsOptions = {
   origin: [BACKOFFICE_CLIENT_HOST, DELIVERY_CLIENT_HOST],
 };
+
 app.use(morgan("dev"));
 app.use(cors(options));
 app.use(json());
 app.use(urlencoded({ extended: false }));
-
 app.use("/", allRoutes);
 
-app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
-  res.status(500).send(error.message);
-});
-
-app.post("/testHistory", async (req: Request, res: Response) => {
-  try {
-    const newDay = await History.create(req.body);
-    newDay.save();
-    res.send(newDay);
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
-    res.sendStatus(500);
+app.use(
+  (error: APIError, _req: Request, res: Response, _next: NextFunction) => {
+    res.status(error.status).send({
+      status: error.status,
+      message: error.message,
+      data: null,
+    });
   }
-});
+);
 
 if (
   process.env.NODE_ENV === "production" ||
