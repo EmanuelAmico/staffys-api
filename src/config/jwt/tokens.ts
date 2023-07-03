19d47d5775ebc34ac17b0b9419733d8833abb6e3
Santiago Estevez <elbolaestevez@gmail.com>
2023-07-02 22:37:41 -0300

    fix middleware and token

--- src/config/jwt/tokens.ts
+++ src/config/jwt/tokens.ts
@@ -4,13 +4,18 @@ import { APIError } from "../../utils/error.utils";
 
 interface Payload {
   _id: Types.ObjectId;
+  is_admin: boolean;
 }
 
 const generateToken = (payload: Payload): string => {
   const secret = process.env.JWT_SECRET as string;
-  const token = sign({ user: payload._id }, secret, {
-    expiresIn: "20d",
-  });
+  const token = sign(
+    { user: { _id: payload._id, is_admin: payload.is_admin } },
+    secret,
+    {
+      expiresIn: "20d",
+    }
+  );
   if (!token)
     throw new APIError({
       message: "token is not generated",
@@ -20,7 +25,8 @@ const generateToken = (payload: Payload): string => {
   return token;
 };
 
-const validateToken = (token: string) =>
-  verify(token, process.env.JWT_SECRET as string);
-
+const validateToken = (token: string) => {
+  const user = verify(token, process.env.JWT_SECRET as string);
+  return user;
+};
 export { generateToken, validateToken };
